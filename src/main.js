be7fc59f35a76510fe69fb9ac6d74b623efaef89
nicoelcy <nicolecy22@gmail.com>
2020-07-10 16:22:00 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,78 @@
+let html = document.querySelector("#html"); //放HTML
+let style = document.querySelector("#style"); //放CSS
+let string = `
+/* 你好，我叫小橙子
+* 接下来我要展示一下我的前端功底
+* 首先我要准备一个div
+**/
+#div1{
+    border: 1px solid red;
+    width: 200px;
+    height: 200px;
+}
+/* 接下来我把 div 变成一个八卦图
+*  注意看好了
+*  首先，把 div 变成一个圆
+**/
+#div1{
+  border-radius: 50%;
+  box-shadow: 0 0 3px rgba(0,0,0,0.5);
+  border: none;
+}
+/* 八卦是阴阳两极形成
+*  一黑一白
+**/
+#div1{
+  background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/* 添加灵珠和魔丸 */
+  #div1::before{
+    width: 100px;
+    height: 100px;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: white;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%);
+}
+#div1::after{
+  width: 100px;
+  height: 100px;
+  bottom: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+  background: black;
+  border-radius: 50%;
+  background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%);
+  }
+
+`;
+
+let string2 = "";
+let n = 0;
+
+let step = () => {
+  setTimeout(() => {
+    // 如果是回车，就不能照搬
+    // 如果不是回车就照搬
+    if (string[n] === "\n") {
+      string2 += "<br>";
+    } else if (string[n] === " ") {
+      string2 += "&nbsp;";
+    } else {
+      string2 += string[n];
+    }
+    html.innerHTML = string2;
+    style.innerHTML = string.substring(0, n);
+    window.scrollTo(0, 99999);
+    html.scrollTo(0, 99999);
+    if (n < string.length - 1) {
+      // 如果n不是最后一个,就继续
+      n += 1;
+      step();
+    }
+  }, 10);
+};
+
+step(); //1=>2
